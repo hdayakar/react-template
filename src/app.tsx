7d6002bffa959c9533838d4406281124af97c004
dayakar <hdayakar@gmail.com>
2021-03-31 21:14:37 +0530

    more changes

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,11 @@
+import './styles.css';
+import IMAGE from './react-icon.png';
+import LOGO from './svg-icon.svg';
+
 export const App = () => {
-	return <h1>React Typescript Webpack Starter Template</h1>
+	return <>
+		<h1>React Typescript Webpack Starter Template</h1>
+		<img src={IMAGE} alt="React Logo" width="300" height="200"/>
+		<img src={LOGO} alt="SVG Icon" width="300" />
+	</>
 }
